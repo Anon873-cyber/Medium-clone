c1764bd039293e3b1116b430507bef5c8697e330
Anon873-cyber <async2304@gmail.com>
2026-04-24 23:27:15 +0530

    refactor: update Like and Subscriber models to use ObjectId for user references

--- Backend/model/Like.Model.js
+++ Backend/model/Like.Model.js
@@ -1,16 +1,24 @@
+import { Blog } from './Blog.Models';
+
 const mongoose = require('mongoose');
 
 const LikeSchema = new mongoose.Schema(
-{
-    postId: ObjectId,
-    userId: String,
-    type: "like" | "dislike"
-}
-, { timestamps: true });
+    {
+        postId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: Blog
+        },  // post where people commented 
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },   // who commented
+        type: "like" | "dislike"
+    }
+    , { timestamps: true });
 
 const Like = mongoose.model('Like', LikeSchema)
 
 
-export{
+export {
     Like
 }
