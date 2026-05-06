a1dec194548a941dc1c05ab900806fb6241fb5f0
Anon873-cyber <async2304@gmail.com>
2026-05-06 22:19:10 +0530

    refactor: streamline comment routes by removing redundant code

--- Backend/routes/Comment.routes.js
+++ Backend/routes/Comment.routes.js
@@ -10,40 +10,12 @@ import {
     updatesinglecommnentController,
 }from "../controller/comment.controller.js"
 
-
-
 route.route(requireAuth)
 
-router.route("/:blogId/comments")
-.get(getcommentsController)
-.post(createcommentController)
-
-router.route("/comments/:id")
-.get(getsingleCommentController)
-.patch(updatesinglecommnentController)
-.delete(deletecommentController)
-
-// Create + Get comments for a blog
-router.route("/:blogId/comments")
-    .post((req, res) => {
-        res.send(`Create comment for blog ${req.params.blogId}`);
-    })
-    .get((req, res) => {
-        res.send(`Get all comments for blog ${req.params.blogId}`);
-    });
-
-// Single comment operations
-router.route("/comments/:id")
-    .get((req, res) => {
-        res.send(`Get comment ${req.params.id}`);
-    })
-    .put((req, res) => {
-        res.send(`Update comment ${req.params.id}`);
-    })
-    .delete((req, res) => {
-        res.send(`Delete comment ${req.params.id}`);
-    });
-
-
+router.route("/:blogId/comments").get(getcommentsController)
+router.route("/:blogId/comments").post(createcommentController)
+router.route("/comments/:id").get(getsingleCommentController)
+router.route("/:blogId/comments").patch(updatesinglecommnentController)
+router.route("/:blogId/comments").delete(deletecommentController)
 
 export default router;
